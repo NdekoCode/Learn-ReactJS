f18166785d9c99ad3f06570547c2ce806b9a681f
NdekoCode <arickbulakali@gmail.com>
2022-07-19 16:13:43 +0200

    add: interpollation variable in JSX syntax

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,12 @@
 let n = 0;
+function numberFormat(nb) {
+  return nb.toString().padStart(2, 0);
+}
 function render() {
   const h1 = (
-    <h1>
-      Bonjour tous le monde <span>{n}</span>
+    <h1 id={"title" + n}>
+      Bonjour tous le monde{" "}
+      <span className={"span" + (n % 2)}>{numberFormat(n)}</span>
     </h1>
   );
   ReactDOM.render(h1, document.getElementById("app"));
