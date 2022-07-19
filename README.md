3de43ee14db6cdd264670551ae96c59c7068e075
NdekoCode <arickbulakali@gmail.com>
2022-07-19 15:46:43 +0200

    add: react learn without jsx syntaxe

---
+++ README.md
@@ -0,0 +1,3 @@
+# Learn ReactJS and discover it
+
+React utilise un système de template appeler le JSX pour faciler les developpeurs dans l'ecriture de leurs programmes React, c'est une extension syntaxique de JavaScript utiliser afin de décrire à quoi devrait ressembler l'interface utilisateur (UI).
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,15 @@
-const h1 = React.createElement("h1", {}, "Bonjour tous le monde");
-console.log(h1);
-ReactDOM.render(h1, document.getElementById("app"));
+let n = 0;
+function render() {
+  const h1 = React.createElement(
+    "h1",
+    {},
+    "Bonjour tous le monde",
+    React.createElement("span", {}, n)
+  );
+  ReactDOM.render(h1, document.getElementById("app"));
+}
+
+window.setInterval(() => {
+  n++;
+  render();
+}, 1000);
