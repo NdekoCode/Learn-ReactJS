257f53c2c93940379d6b953e480c21a43fee81e3
NdekoCode <arickbulakali@gmail.com>
2022-07-21 18:03:05 +0200

    use state and setState in a Clock Component

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,82 @@
-let n = 0;
-function numberFormat(nb) {
-  return nb.toString().padStart(2, 0);
-}
-function render() {
-  // Quand vous essayer de creer plusieurs elements sans forcement qu'il y est plusieurs racine definit
-  const items = ["Tache 1", "Tache 2", "Tache 3", "Tache 4", "Tache 5"];
-  const lis = items.map((item, index) => <li key={index}>{item}</li>);
-  const h1 = (
-    <React.Fragment>
-      <h1 id={"title" + n}>
-        Bonjour tous le monde{" "}
-        <span className={"span" + (n % 2)}>{numberFormat(n)}</span>
-      </h1>
-      <ul>{lis}</ul>
-    </React.Fragment>
+/** les Composants Reacts */
+// On va créer un titre qui va dire bonjour suivis du nom de la personne et on va utiliser un h1 pour cela
+const namesClient = ["Jean", "Cedric", "Benjamin", "Reactif"];
+
+function WelcomeFun({ names, children }) {
+  console.log(names);
+  return (
+    <div>
+      <ul className="list">
+        {names.map((item, k) => (
+          <li key={k}>Bonjour {item}</li>
+        ))}
+      </ul>
+      <p> {children}</p>
+    </div>
   );
-  ReactDOM.render(h1, document.getElementById("app"));
 }
 
-window.setInterval(() => {
-  n++;
-  render();
-}, 1000);
+class Welcome extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log(props);
+  }
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.props.names.map((item, k) => (
+            <li key={k}>Bonjour {item}</li>
+          ))}
+        </ul>
+        <p>{this.props.children}</p>
+      </div>
+    );
+  }
+}
+
+function Home() {
+  return (
+    <div>
+      <Welcome names={namesClient}>
+        Bonjour les gars j'espere que vous allez bien
+      </Welcome>
+      <Clock />
+    </div>
+  );
+}
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.timer = null;
+    this.state = { date: new Date() };
+  }
+  componentDidMount() {
+    this.timer = window.setInterval(() => {
+      this.tick();
+    }, 1000);
+  }
+  componentWillUnmount() {
+    window.clearInterval(this.timer);
+  }
+  /**
+   * @description Une methode qui va changer l'Etat
+   * @author NdekoCode
+   * @memberof Clock
+   */
+  tick() {
+    // Pour changer l'etat on utilise la methode setState et on lui passe un objet qui represente le nouvel etat
+    this.setState({ date: new Date() });
+  }
+  render() {
+    return (
+      <div>
+        Il est {this.state.date.toLocaleDateString()}{" "}
+        {this.state.date.toLocaleTimeString()}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Home />, document.getElementById("app"));
