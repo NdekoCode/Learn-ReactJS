77e6979d57475d39949c6a6d822706c9ac3922a2
NdekoCode <arickbulakali@gmail.com>
2022-07-22 04:57:51 +0200

    additionnality of similtanery event click on our Increment component

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,17 +5,46 @@ class ManualIncrement extends React.Component {
   };
   constructor(props) {
     super(props);
-    this.state = { index: props.start };
-    console.log(this.state, props);
+    this.state = { index: props.start, timer: null };
+  }
+  componentDidMount() {
+    this.play();
+  }
+  componentWillUnMount() {
+    // On ne doit pas appeler un setState dans cet evenement pour eviter d'avoir des problèmes internes avec React
+    window.clearInterval(this.state.timer);
+  }
+  /**
+   * @description Va incrementer l'index
+   * @author NdekoCode
+   * @memberof ManualIncrement
+   */
+  increment() {
+    this.setState((state, props) => ({ index: state.index + props.step }));
+  }
+  play() {
+    window.clearInterval(this.state.timer);
+    // On est pas obliger de modifier "index" car il va fusionner l'objet retourner avec l'Etat courant
+    this.setState({
+      timer: window.setInterval(this.increment.bind(this), 1000),
+    });
+  }
+  pause() {
+    window.clearInterval(this.state.timer);
+    this.setState({ timer: null });
   }
 
   render() {
     return (
-      <button onClick={this.increment.bind(this)}>{this.state.index}</button>
+      <div>
+        valeur: <strong>{this.state.index}</strong>
+        {this.state.timer ? (
+          <button onClick={this.pause.bind(this)}>Pause</button>
+        ) : (
+          <button onClick={this.play.bind(this)}>Lecture</button>
+        )}
+      </div>
     );
-  }
-  increment() {
-    this.setState((state, props) => ({ index: state.index + props.step }));
   }
 }
 function Home() {
