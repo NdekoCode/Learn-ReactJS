5275bae083347910fb0744d429da5b5489dac8ea
NdekoCode <arickbulakali@gmail.com>
2022-08-07 21:22:50 +0200

    add documentation to temperature converter

--- assets/js/celcius-fahrenheit.js
+++ assets/js/celcius-fahrenheit.js
@@ -29,8 +29,14 @@ function BoilingVerdict({ celcius }) {
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
+
+    /**
+     * @temperature {integer} represente la valeur entrer dans le composant TemperatureInput
+     */
     this.state = {
       temperature: 0,
+      // Pour que notre calculator puisse savoir sur quelle échelle il est
+      scale: "c",
     };
     this.handleCelciusChange = this.handleCelciusChange.bind(this);
     this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
@@ -51,10 +57,18 @@ class Calculator extends React.Component {
   }
   render() {
     const { temperature, scale } = this.state;
+    // Si l"echel est en "Celcius" alors renvoyer mois la température normal, sinon convertis la temperature rentrer en farheinet en degrer celcius
     const celcius =
       scale === "c" ? temperature : fahrenheitToCelcius(temperature);
+    // Si l"echel est en "fahrenheit" alors renvoyer mois la température normal EN fahrenheit, sinon convertis la temperature QUI est rentrer en celcius en farheinet en degrer celcius
+
     const fahrenheit =
       scale === "f" ? temperature : celciusToFahrenheit(celcius);
+
+    /**
+     * L'attribut onTemperatureChange permet de faire en sorte que le Calculator puisse detecter quand t'on a changer la temperature dans le composant TemperatureInput
+     */
+
     return (
       <div>
         <TemperatureInput
@@ -80,6 +94,8 @@ class TemperatureInput extends React.Component {
     this.temperatureHandleChange = this.temperatureHandleChange.bind(this);
   }
   temperatureHandleChange({ target }) {
+    µ;
+    // La proprieter onTemperatureChange permet de faire remonter les informations de l'enfant aux parents, c'est un callback utiliser comme une proprieter sur le parent et ce qui est donné comme valeur à a la proprieter declencherant un nouveau rendus à la detection de ce dernier
     this.props.onTemperatureChange(target.value);
   }
   render() {
