7b933ab2fda6fec2302cfa8a45e5e1c6461333fb
NdekoCode <arickbulakali@gmail.com>
2022-08-10 18:51:37 +0200

    forward ref with classComponent but depreciated

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,10 +11,10 @@ class Home extends React.Component {
     console.log(this.input);
     return (
       <div className="container mt-5">
-        <div className="form-group d-flex">
-          <Field ref={this.input} />
+        <div className="form-group d-flex align-items-center">
+          <FieldComponentWithRef ref={this.input} label="Demo" />
           <button
-            className="btn btn-secondary rounded-0 rounded-end"
+            className="btn btn-secondary rounded-0 h-100 rounded-end"
             onClick={this.handleClick}
           >
             Valider
@@ -25,15 +25,25 @@ class Home extends React.Component {
   }
 }
 
-const Field = React.forwardRef(function (props, ref) {
-  return (
-    <div className="form-group">
-      <input
-        type="text"
-        className="form-control rounded-0 rounded-start"
-        ref={ref}
-      />
-    </div>
-  );
+const FieldComponentWithRef = React.forwardRef((props, ref) => {
+  // {...props} permet de dire que toute proprieter passer à props sera pris comme un enfant de props ainis si on met un <FieldComponent label="Demo"/> il sera directement rajouter dans les props sous forme this.props = {label:"Demo"}
+  return <Field forwardRef={ref} {...props} />;
 });
+class Field extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="form-group">
+        {/* <label htmlFor="">{this.props.label}</label> */}
+        <input
+          type="text"
+          className="form-control rounded-0 rounded-start"
+          ref={this.props.forwardRef}
+        />
+      </div>
+    );
+  }
+}
 ReactDOM.render(<Home />, document.getElementById("app"));
