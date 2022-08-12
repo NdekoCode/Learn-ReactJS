a0bb9de99abad00b1a16dc54664a9c2ac3d9705c
NdekoCode <arickbulakali@gmail.com>
2022-08-12 21:28:41 +0200

    resolve problem  modify state with using a function callback which the previous state is the first argument

--- src/components/Compter.jsx
+++ src/components/Compter.jsx
@@ -4,7 +4,12 @@ export function Compter() {
   let [state, setState] = useState({ id: 1 });
   const handleClick = (evt) => {
     evt.preventDefault();
-    setState({ count: 10 });
+    setState((state) => {
+      return {
+        ...state,
+        count: 10,
+      };
+    });
   };
   return (
     <div className="compter">
