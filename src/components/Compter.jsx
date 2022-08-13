c5825b5761675543937d87f5fd08d07e9ec62e6b
NdekoCode <arickbulakali@gmail.com>
2022-08-13 06:07:46 +0200

    personal externe hook for a clean code

--- src/components/Compter.jsx
+++ src/components/Compter.jsx
@@ -1,20 +1,27 @@
 import React, { useState } from "react";
 import "../assets/css/Compter.css";
+function useIncrement() {
+  const [count, setCount] = useState(0);
+  const increment = () => {
+    setCount((count) => count + 1);
+  };
+  return [count, increment];
+}
 export function Compter() {
-  let [state, setState] = useState({ id: 1 });
+  let [state, setState] = useState({ count: 1 });
   const handleClick = (evt) => {
     evt.preventDefault();
     setState((state) => {
       return {
         ...state,
-        count: 10,
+        count: state.count + 1,
       };
     });
   };
   return (
     <div className="compter">
       <button onClick={handleClick}>
-        Nombre compter: {JSON.stringify(state)}
+        Nombre compter:{state.count} {JSON.stringify(state)}
       </button>
     </div>
   );
