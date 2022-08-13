0be3d8f5109dc1a0365f288be40b7e295c586ef8
NdekoCode <arickbulakali@gmail.com>
2022-08-13 09:10:25 +0200

    useEffect with full parameters contain

--- src/components/Compter.jsx
+++ src/components/Compter.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../assets/css/Compter.css";
 function useIncrement(init = 0, step = 1) {
   const [count, setCount] = useState(init);
@@ -9,6 +9,10 @@ function useIncrement(init = 0, step = 1) {
 }
 export function Compter() {
   let [count, increment] = useIncrement(3, 2);
+  useEffect(() => {
+    // depend de l’Etat du state du composant sur lequel il s'execute
+    document.title = "Compteur " + count;
+  }, [count]);
   return (
     <div className="compter">
       <button onClick={increment}>Nombre compter:{count}</button>
