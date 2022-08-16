6b4fd01a7dff8ed958c8cb8bae4e3d00dffb97a3
NdekoCode <arickbulakali@gmail.com>
2022-08-16 06:33:20 +0200

    add useRef for stock infos permanmly in react lifecycle and get DOM input value

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useRef } from "react";
 // import { AutoCompter } from "./Compter";
 
 import { useToggleCompterVisible } from "../hooks/Hooks";
 import { Encode } from "./Encode";
+import { UnControlInput } from "./UnControlInput";
 export function Home() {
   const [compterVisible, toggleCompter] = useToggleCompterVisible();
   return (
     <div className="container mt-5">
+      <div className="mt-5 mb-3">
+        <UnControlInput />
+      </div>
       <div>
         Afficher le compteur:
         <input
