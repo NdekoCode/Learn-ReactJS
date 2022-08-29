16f9547378ef1517b83a96b31f1fc57921dbf29c
NdekoCode <arickbulakali@gmail.com>
2022-08-29 21:07:40 +0200

    animate children element by css class and is DOM removing

--- src/components/Fade.jsx
+++ src/components/Fade.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 /**
  * @description est un composant qui va permettre à ses enfant de se cacher ou de se rendre visible avec un effet d'opacité progressif
  * @param {Object} props
@@ -8,9 +10,20 @@ export const Fade = ({visible,children})=>{
 
 // On va activer ou desactiver cette clase "out" si l'on veut une animation
     let className ="fade__class";
+    const [showChildren,setShowVisible] = useState(visible);
+    useEffect(()=>{
+        if(visible){
+            // SI l'element est visible alors il reste visible
+            setShowVisible(true)
+        }else {
+            // Sinon on rend cet element invible après 350ms càd 0.35s
+            setTimeout(()=>setShowVisible(false),350);
+
+        }
+    },[visible])
     // Si l'element n'est pas visible alors on ajoute au niveau de la classe la classe "out"
     if(!visible) {
         className+=" out";
     }
-    return <div className={className}>{children}</div>
+    return <div className={className}>{showChildren && children}</div>
 }
