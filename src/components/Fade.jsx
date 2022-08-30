f9461beeac69c0eafb84203823749ed236cd3dc7
NdekoCode <arickbulakali@gmail.com>
2022-08-30 10:30:09 +0200

    add: animation with removing children without removing parent which contain fade class

--- src/components/Fade.jsx
+++ src/components/Fade.jsx
@@ -10,17 +10,22 @@ export const Fade = ({visible,children})=>{
 
 // On va activer ou desactiver cette clase "out" si l'on veut une animation
     let className ="fade__class";
+    //On va créer un Etat qui va permettre de dire si oui ou non on doit afficher l'enfant
     const [showChildren,setShowVisible] = useState(visible);
+    // Quand le composant est monté on verifie si on doit afficher l'enfant ou le cacher
     useEffect(()=>{
         if(visible){
-            // SI l'element est visible alors il reste visible
+            // SI l'element est visible alors il reste visible dans le DOM
             setShowVisible(true)
         }else {
             // Sinon on rend cet element invible après 350ms càd 0.35s
-            setTimeout(()=>setShowVisible(false),350);
+            const timer = setTimeout(()=>setShowVisible(false),350);
+            return ()=>{
+                clearTimeout(timer);
+            }
 
         }
-    },[visible])
+    },[visible])// On verifie la valeur de visible à chaque fois
     // Si l'element n'est pas visible alors on ajoute au niveau de la classe la classe "out"
     if(!visible) {
         className+=" out";
