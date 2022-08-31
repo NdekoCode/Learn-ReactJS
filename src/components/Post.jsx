8779e9084e76305a6c2237e8946c82a390c502dd
NdekoCode <arickbulakali@gmail.com>
2022-08-31 22:17:08 +0200

    refactor: Post component for a clean code

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,23 +3,29 @@ import { connect } from "react-redux";
 import { useParams } from "react-router-dom";
 import { Fade } from "./Fade";
 
-const Post = ({post}) => {
+const Post = ({ post }) => {
   return (
     <Fade visible={true}>
       <div className="container">
-        <h2 className="m-1 mb-3">{post.title}</h2>
-        <p>{post.body}</p>
+        {post ? (
+          <div>
+            <h2 className="m-1 mb-3">{post.title}</h2>
+            <p>{post.body}</p>
+          </div>
+        ) : (
+          <p>L'article n'existe pas</p>
+        )}
       </div>
     </Fade>
   );
 };
-export const PostStore = connect(
-  (state) => {
-    const params = useParams();
-    let id = parseInt(params.id);
-    
+export const PostStore = connect(({ posts }) => {
+  // On recupère les paramètres dans l'URL
+  const params = useParams();
+  // On transforme l'id dans l'url en entier
+  let id = parseInt(params.id);
 
-    return{ post: state.posts.find(post=>post.id===id)};
-  }
-)(Post);
+  // On récupère le post qui correpond parmis les posts qu'on a dans le state
+  return { post: posts.find((post) => post.id === id) };
+})(Post);
 export default PostStore;
