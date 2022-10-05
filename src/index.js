2bf1ea17f1f6fd9d531c90a7292642e3f4495f04
NdekoCode <arickbulakali@gmail.com>
2022-10-05 17:37:23 +0200

    add styled-components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 // import { MakeAnimation } from "./components/MakeAnimation";
-import "./assets/css/app.css";
 // import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
