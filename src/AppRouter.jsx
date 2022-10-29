f6bb7e526fe2aaf3c2d74e1abd834597f73a6800
NdekoCode <arickbulakali@gmail.com>
2022-10-29 07:32:01 +0200

    learn(donkey): context with Provider and Consumer

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Docs from "./components/Docs";
 import Tutoriels from "./components/Tutoriels";
 import Community from "./components/Community";
@@ -6,6 +6,7 @@ import Navbar from "./donkeyGeek/Navbar";
 import { Routes, Route } from "react-router-dom";
 import NotFoundPage from "./donkeyGeek/NotFoundPage";
 import Profile from "./donkeyGeek/Profile";
+import MyContext from "./donkeyGeek/MyContext";
 
 const AppRouter = () => {
   const [underConst, setUnderConst] = useState({
@@ -13,24 +14,30 @@ const AppRouter = () => {
     Tutorials: true,
     Community: true,
   });
+  const [user, setUser] = useState({
+    name: "Ndekocode",
+    age: 8,
+  });
   return (
     <div className="text-center">
       <Navbar />
 
       <h1 className="text-2xl font-bold text-gray-700">Goku vs Vegeta</h1>
-      <div className="flex flex-col">
-        <Routes>
-          <Route path="/" element={<Docs />} />
-          {/* <Route path="/tutoriel" element={<Tutoriels />} /> */}
-          <Route path="/tutoriel" element={<Tutoriels />} />
-          <Route
-            path="/community"
-            element={<Community render={underConst.Community} />}
-          />
-          <Route path="/profile/:id" element={<Profile />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </div>
+      <MyContext.Provider value={user}>
+        <div className="flex flex-col">
+          <Routes>
+            <Route path="/" element={<Docs />} />
+            {/* <Route path="/tutoriel" element={<Tutoriels />} /> */}
+            <Route path="/tutoriel" element={<Tutoriels />} />
+            <Route
+              path="/community"
+              element={<Community render={underConst.Community} />}
+            />
+            <Route path="/profile/:id" element={<Profile />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </div>
+      </MyContext.Provider>
     </div>
   );
 };
